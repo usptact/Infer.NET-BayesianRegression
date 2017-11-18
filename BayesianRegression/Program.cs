4750eb3287b18ee71a8bc5e5e193a117f2cc0eee
Vladislavs Dovgalecs <usptact@inbox.lv>
2017-11-17 23:50:20 -0800

    Add project files.

---
+++ BayesianRegression/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BayesianRegression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
